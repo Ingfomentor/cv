3ae5642d3480e04d454fde8188f37075e0ce30d1
Christophe VG <xtof@pumpkin.local>
2013-02-13 14:54:27 +0100

    implemented second assignment (smoothing with OpenCV)

--- project1/smoothing2.py
+++ project1/smoothing2.py
@@ -6,39 +6,39 @@ import cv2
 import math
 import numpy as np
    
-def gaussian_smooth2(img, sigma): 
-    '''
-    Do gaussian smoothing with sigma.
-    Returns the smoothed image.
-    '''
-    result = np.zeros_like(img)
-    
-    #determine the length of the filter
-    filter_length= math.ceil(sigma*5) 
-    #make the length odd
-    filter_length= 2*(int(filter_length)/2) +1  
-            
-    #Go ahead! 
-    #Tip: smoothing=blurring, a filter=a kernel
-    print('TODO')
-    
-    return result
+def gaussian_smooth2(image, sigma): 
+  '''
+  Do gaussian smoothing with sigma.
+  @param image  2D array representing 2D image with layer for each color (RGB)
+  @param sigma  float, defining the width of the filter
+  @return the smoothed image.
+  '''
+  # filter = np.zeros_like(image)
 
+  # determine the length of the filter
+  filter_length = math.ceil(sigma*5) 
+  # make the length odd
+  filter_length = 2*(int(filter_length)/2) +1  
 
+  # create filter
+  filter = cv2.getGaussianKernel(filter_length, sigma)
+  # apply filter
+  result = cv2.sepFilter2D(image, -1, filter, filter)
 
-#this part of the code is only executed if the file is run stand-alone
+  return result
+
+# this part of the code is only executed if the file is run stand-alone
 if __name__ == '__main__':
-    #read an image
-    img = cv2.imread('image.png')
-    
-    #show the image, and wait for a key to be pressed
-    cv2.imshow('img',img)
-    cv2.waitKey(0)
-    
-    #smooth the image
-    smoothed_img = gaussian_smooth2(img, 2)
-    
-    #show the smoothed image, and wait for a key to be pressed
-    cv2.imshow('smoothed_img',smoothed_img)
-    cv2.waitKey(0)
-    
+  # read an image
+  image = cv2.imread('image.png')
+
+  # show the image, and wait for a key to be pressed
+  cv2.imshow('image',image)
+  cv2.waitKey(0)
+
+  # smooth the image
+  smoothed_image = gaussian_smooth2(image, 2)
+
+  # show the smoothed image, and wait for a key to be pressed
+  cv2.imshow('smoothed_image',smoothed_image)
+  cv2.waitKey(0)
