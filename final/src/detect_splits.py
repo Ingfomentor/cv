d61a4fd3e666891687d9fa39e55971307a2aba21
Christophe VG <xtof@astroboy.local>
2013-04-29 12:35:57 +0200

    improved application of Gaussian probability

--- final/src/detect_splits.py
+++ final/src/detect_splits.py
@@ -18,65 +18,123 @@ import repository as repo
 
 '''
 TODO
-- implement piece-wise + spline function
+- implement spline combination of splits
+- clean up split ? splits !
+- MAGIC NUMBER 1.4 invert and SHIFT = explain
 '''
 
-def find_horizontal_splits(image):
+def find_horizontal_splits(image, left=0, width=None, previous=None):
   '''
   Sums the intensities of each row, applying a Gaussian probability-based factor
   to darken centered-regions (with a bias of 11 percent down) and to lighten
   less probable areas.
   @param image to detect split(s) in
+  @param left position to start slice
+  @param width width of the slice (default=width image)
+  @param previous detected (=expected) height
   @return lowest intensity value(s), representing the height of the split(s)
   '''
   
+  # default to whole image
+  if width == None: _, width, _ = image.shape
+
+  # default expected to middle of image (vertically)
+  height, _, _ = image.shape
+  if previous == None: previous = height / 2
+
   # sum each row to get row intensity
-  intensities = np.sum(image[:,:,1], 1)
+  intensities = np.sum(image[:,left:left+width,1], 1)
 
   # apply a Gaussian-based factor to lighten top/bottom region and darken 
   # central regions, equal to the probability where the split occurs
   count = len(intensities)
   factors = np.zeros(count)
+  p = (float(previous) - (float(count)/2) ) / float(count) # convert to -1..1 range
   for y in range(count):
-    yy = ( float(y) - (float(count)/2) ) / float(count) # conver to -1..1 range
-    g = gaussian_value(yy-0.11)                         # 11% bias downwards
-    factors[y] = 1.4 - g                                # invert and shift
+    x = ( float(y) - (float(count)/2) ) / float(count) # convert to -1..1 range
+    g = gaussian_value(x, p) 
+    factors[y] = 1.4 - g             # invert and shift
 
   # apply probability correction factors
   intensities = np.multiply(intensities, factors)
 
-  # find lowest values = jaw separation valleys
-  return np.argmin(intensities)
+  # find lowest value(s) = jaw separation valley(s)
+  darkest = np.argmin(intensities)
+  
+  return darkest
 
-def gaussian_value(x):
+def gaussian_value(x, expected):
   '''
   Computes a Gaussian value for a given x value between -1 and 1
   @param x value between -1 and 1
+  @param expected location = top of Gaussian curve
   @return Gaussian value for x
   '''
-  return ( math.e ** - (x ** 2 * 10) ) / math.sqrt(2*math.pi*0.1)
+  rho = 0.3
+  return ( math.e ** - ( ((x-expected) ** 2) / rho ** 2 ) ) \
+         / \
+         (math.sqrt(2*math.pi) * rho)
 
-def process(image):
+def process(image, slices=7, start=None):
   '''
   Process a given image, detecting the horizontal split(s) and annotating them
   on the image.
   @param image to process
+  @param slices to apply to image = number of splits
   @return processed image, annotated with split(s)
   '''
-  
-  # find horizontal splits
-  splits = find_horizontal_splits(image)
 
-  # show lines indicating valleys
-  _, width, _ =  image.shape
-  for split in np.nditer(splits):
-    pt1 = (0,     split)
-    pt2 = (width, split)
-    cv2.line(image, pt1, pt2, (0,255,0), 3)
+  # make sure that the number of slices is odd to have a clear middle slice
+  assert slices % 2 == 1
+
+  height, width, _ =  image.shape
+  slice_width = width / slices
+  
+  if start == None: start = 0.5   # default: expect split in the middle of image
+
+  prev_split = height * start
+
+  # start with middle split and move outwards left/right
+  middle = slices / 2   # 7 / 2 = 3 == middle (0..6)
+  current_split = middle
+  splits = find_horizontal_splits(image, current_split * slice_width, slice_width, prev_split)
+  image  = draw_split(image, splits, current_split, slice_width)
+  prev_left_split  = splits
+  prev_right_split = splits
+  while current_split >= 0:
+    current_split = current_split - 1
+
+    # go left
+    left_splits = find_horizontal_splits(image, current_split * slice_width, slice_width, prev_left_split)
+    image = draw_split(image, left_splits, current_split, slice_width)
+    prev_left_split = left_splits
+    
+    # and right
+    right_splits = find_horizontal_splits(image, (slices - 1 - current_split) * slice_width, slice_width, prev_right_split)
+    image = draw_split(image, right_splits, slices - 1 - current_split, slice_width)
+    prev_right_split = right_splits
   
   return image
 
-def show(image):
+def draw_split(image, splits, index, width, color=(0,255,0), line_width=3 ):
+  '''
+  Draws a line on an image, representing a partial split, index-based
+  @param image on which to draw
+  @param splits to draw
+  @param index of slice
+  @param width of slice
+  @param color to draw line with (default=green)
+  @param line_width to draw the line with (default=3pixels)
+  @return image with line(s) drawn onto
+  '''
+  # show lines indicating splits/valleys
+  for split in np.nditer(splits):
+    pt1 = (index * width, split)
+    pt2 = ((index+1) * width, split)
+    cv2.line(image, pt1, pt2, color, line_width)
+  return image
+
+def show(image, start=None):
   '''
   Shows the original and the annotated image with split(s)
   @param image to show and process
@@ -86,7 +144,7 @@ def show(image):
   cv2.waitKey(0)
   
   # show annotated splits
-  process(image)
+  process(image, start=start)
   cv2.imshow("splits", image)
   cv2.waitKey(0)
 
@@ -97,22 +155,24 @@ if __name__ == '__main__':
   output_file = None
 
   # obtain arguments
-  if len(sys.argv) < 2:
-    print "!!! Missing arguments, please provide at least and input filenames."
+  if len(sys.argv) < 3:
+    print "!!! Missing arguments, please provide at least an input and reference."
     sys.exit(2)
-  elif len(sys.argv) < 3:
+  elif len(sys.argv) < 4:
     image_file  = sys.argv[1]
+    start       = float(sys.argv[2])
   else:
     image_file  = sys.argv[1]
     output_file = sys.argv[2]
+    start       = float(sys.argv[3])
   
   # read image (this should be an enhanced grayscale image)
   image = repo.get_image(image_file)
   assert image.dtype == "uint8"
 
   if output_file != None:
-    annotated = process(np.copy(image))
+    annotated = process(np.copy(image), start=start)
     # save a copy with visual 
     repo.put_image(output_file, annotated)
   else:
-    show(image)
+    show(image, start)
