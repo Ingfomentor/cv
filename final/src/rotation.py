b9376f34e4fee758827cace53f6f4e7bb955ccf9
Christophe VG <xtof@astroboy.local>
2013-05-19 17:26:00 +0200

    extracted rotate_points function

--- final/src/rotation.py
+++ final/src/rotation.py
@@ -20,3 +20,16 @@ def get_angle(pt1, pt2):
 def get_rotation_matrix(angle):
   return np.array( [ [ math.cos(angle), - math.sin(angle) ],
                      [ math.sin(angle),   math.cos(angle) ] ] )
+
+def rotate_points(points, angle, center = (0,0)):
+  points = np.copy(points)
+
+  # make center = (0,0)
+  points = points - center
+  # rotate
+  rotation_matrix  = get_rotation_matrix(angle)
+  points = np.dot(points, rotation_matrix.T)
+  # move back
+  points = points + center
+
+  return points
