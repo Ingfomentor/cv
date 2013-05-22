3f5f8f2aa3e1a40febebe84633261c25b67af0f1
Christophe VG <xtof@astroboy.local>
2013-05-22 10:28:39 +0200

    extracted create_histogram function

--- final/src/create_histogram.py
+++ final/src/create_histogram.py
@@ -7,27 +7,31 @@ Creates Histograms for given image, saving it in Matlab format
 
 import sys
 import cv2
-import scipy.io as sio
 
 import repository as repo
 
 
-# obtain arguments
-if len(sys.argv) < 3:
-  print "!!! Missing arguments, please provide input and output filenames."
-  sys.exit(2)
+def create_histogram(image):
+  assert image.dtype == "uint8"
+  return cv2.calcHist([image], [0], None, [256], [0,255])
 
-image_file  = sys.argv[1]
-output_file = sys.argv[2]
 
-# read image
-image = repo.get_image(image_file)
+# this part of the code is only executed if the file is run stand-alone
+if __name__ == '__main__':
 
-# convert to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  # obtain arguments
+  if len(sys.argv) < 3:
+    print "!!! Missing arguments, please provide input and output filenames."
+    sys.exit(2)
 
-# create histogram
-histogram = cv2.calcHist([gray], [0], None, [256], [0,255])
+  image_file  = sys.argv[1]
+  output_file = sys.argv[2]
+
+  # read image
+  gray = repo.get_grayscale_image(image_file)
+
+  # create histogram
+  histogram = create_histogram(gray)
     
-# save data to disk in Matlab format for further processing using Octave
-repo.put_data(output_file, {'histogram': histogram})
+  # save data to disk in Matlab format for further processing using Octave
+  repo.put_data(output_file, {'histogram': histogram})
