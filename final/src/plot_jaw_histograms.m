84d66f5c98d04315c75bb01ec5fef0e91803d4da
Christophe VG <xtof@astroboy.local>
2013-05-23 17:34:43 +0200

    improved visualisation of jaw_histograms

--- final/src/plot_jaw_histograms.m
+++ final/src/plot_jaw_histograms.m
@@ -12,12 +12,16 @@ function plot_jaw_histograms(data_file, image_file)
     histogram   = histograms(slice,:);
     split_upper = splits_upper(slice);
     split_lower = splits_lower(slice);
+    top = max(histogram) * 1.1
+    len = length(histogram)
 
     subplot(slices, 1, slice)
     plot(histogram);
     hold on
-    plot([split_upper,split_upper],[0,5000], 'r')
-    plot([split_lower,split_lower],[0,5000], 'g')
+    plot([split_upper,split_upper],[0,top], 'r', 'lineWidth', 4)
+    plot([split_lower,split_lower],[0,top], 'g', 'lineWidth', 4)
+    axis([0 len 0 top])
+    legend('summed intensities', 'upper split', 'lower split')
   endfor
 
   print(image_file, '-tight', '-color');
